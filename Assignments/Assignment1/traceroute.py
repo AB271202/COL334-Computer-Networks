7308856d627e7de21def1b8efd7b7dd191c311e7
AB271202 <anishbanerjee2002@gmail.com>
2023-08-09 15:23:18 +0530

    Problem in redering codes

---
+++ Assignments/Assignment1/traceroute.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# import subprocess
+# cmd = [ 'ping', 'arg1', 'arg2' ]
+# output = subprocess.Popen( cmd, stdout=subprocess.PIPE ).communicate()[0]
+# print(output)
+
+site=input("Enter site: ")
