c495bdeb72b1fbc3137381aa5d070b274d051c61
AB271202 <anishbanerjee2002@gmail.com>
2023-08-11 18:10:37 +0530

    Wireshark

--- Assignments/Assignment1/traceroute.py
+++ Assignments/Assignment1/traceroute.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 import os
-site="www.google.com"
+# site="www.google.com"
 
-# site=input("Enter site: ")
+site=input("Enter site: ")
 
 # Getting the IP Address
 if os.system(f"nslookup {site} | awk 'NR == 6'>temp")==0:
@@ -17,19 +17,17 @@ print(f"traceroute to {site} ({IP}), 64 hops max")
 while os.system(f"ping -c 1 -t {i} {site} > temp") and i<=64:
 	with open("temp","r") as f:
 		x=f.readlines()
-		# print(x[1])
+		# print(x)
 		try:
 			if i==1: print(i,"\t",x[1].split()[2][1:-1])
 			else: print(i,"\t",x[1].split()[1])
 		except: print(i,"\t","* * *")
-		print()
+		# print()
 		i+=1
-# print("Finally")
 with open("temp","r") as f:
-		x=f.readlines()
-		# print(x)
-		try: print(i,"\t",x[1].split()[4][1:-2])
-		except: print(i,"\t","* * *")
-		print()
-		i+=1
-os.delete("temp")
+	x=f.readlines()
+	try: print(i,"\t",x[1].split()[4][1:-2])
+	except: print(i,"\t","* * *")
+	# print()
+	i+=1
+os.remove("temp")
