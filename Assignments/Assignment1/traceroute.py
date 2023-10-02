7f5be03f2d5a8d7405ec15055317aec927374ec3
AB271202 <anishbanerjee2002@gmail.com>
2023-10-02 20:38:10 +0530

    Update

--- Assignments/Assignment1/traceroute.py
+++ Assignments/Assignment1/traceroute.py
@@ -44,39 +44,3 @@ while i<=64:
 
 print()
 print("Trace Complete")
-
-# if os.system(f"nslookup {site} | awk 'NR == 6'>temp")==0:
-# 	with open("temp","r") as f:
-# 		x=f.readline()
-# 		IP=x.split()[1]
-
-# print(s.stdout)
-# # while os.system(f"ping -c 1 -t {i} {site} > temp") and i<=64:
-# # 	with open("temp","r") as f:
-# # 		x=f.readlines()
-# # 		# print(x)
-# # 		try:
-# # 			if i==1: print(i,"\t",x[1].split()[2][1:-1])
-# # 			else: print(i,"\t",x[1].split()[1])
-# # 		except: print(i,"\t","* * *")
-# # 		# print()
-# # 		i+=1
-# # with open("temp","r") as f:
-# # 	x=f.readlines()
-# # 	try: print(i,"\t",x[1].split()[4][1:-2])
-# # 	except: print(i,"\t","* * *")
-# # 	# print()
-# # 	i+=1
-# # os.remove("temp")
-
-# while os.system(f"nping -c 3 -ttl {i} {site} > temp") and i<=64:
-# 	with open("temp","r") as f:
-# 		x=f.readlines()
-# 		print(x)
-# 		# try:
-# 		# 	if i==1: print(i,"\t",x[1].split()[2][1:-1])
-# 		# 	else: print(i,"\t",x[1].split()[1])
-# 		# except: print(i,"\t","* * *")
-# 		# print()
-# 		i+=1
-
