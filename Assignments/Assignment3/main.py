1fbd28f05a6f9e5660c942a83c20ad5a533c5fbd
Ankit Mondal <ankitmondal@Ankits-MacBook-Air-6.local>
2023-10-11 21:37:49 +0530

    main somewhat done. errors exist

--- Assignments/Assignment3/main.py
+++ Assignments/Assignment3/main.py
@@ -5,6 +5,7 @@ SNAME = "localhost"
 SPORT=9801
 client=socket(AF_INET, SOCK_DGRAM)
 message="SendSize\nReset\n\n"
+client.settimeout(0.05)
 # message = f"Offset: 0\nNumBytes: 10\n\n"
 for i in range(0,1):
     client.sendto(message.encode(),(SNAME,SPORT))
@@ -14,18 +15,79 @@ print(data.decode())
 flag = True
 k = 1
 message = f"Offset: 0\nNumBytes: 10\n\n"
-while (flag):
+# while (flag):
+#     for i in range(k):
+#         client.sendto(message.encode(),(SNAME,SPORT))
+#         print("Message sent to server",k)
+#         wait = True
+#         resp = ""
+#         while(wait):
+#             # print("back here")
+#             try:
+#                 # print("going here")
+#                 wait = False
+#                 data,addr=client.recvfrom(2048)
+#                 resp = data.decode()
+#                 print("recv")
+#             except:
+#                 wait = True
+#                 client.sendto(message.encode(),(SNAME,SPORT))
+#                 # print("wait")
+#         print(resp)
+#         if ("Squished" in resp):
+#             flag = False
+#             break
+#     time.sleep(0.0001)
+#     if flag:
+#         k+=1
+#     else:
+#         break
+# print("Determined rate",k-1)
+s = 0
+ans = ""
+flag = True
+k = 56
+while (flag and s<=876000):
     for i in range(k):
+        message = f"Offset: {s}\nNumBytes: 1000\n\n"
         client.sendto(message.encode(),(SNAME,SPORT))
-        print("Message sent to server",k)
-        data,addr=client.recvfrom(2048)
-        if ("Squished" in data.decode()):
+        if(s>876000):
+            break
+        print("Message sent to server",s)
+        wait = True
+        resp = ""
+        while(wait):
+            # print("back here")
+            try:
+                # print("going here")
+                wait = False
+                data,addr=client.recvfrom(2048)
+                resp = data.decode()
+                # print("recv")
+            except:
+                wait = True
+                client.sendto(message.encode(),(SNAME,SPORT))
+                # print("wait")
+        # print(resp)
+        if ("Squished" in resp):
             flag = False
             break
-    time.sleep(10)
+        else:
+            fields = resp.split("\n")
+            ans+=fields[3]
+            if (s>876000):
+                flag = False
+                break
+            s+=1000
+    time.sleep(0.0001)
     if flag:
-        k+=1
+        k += 1
     else:
-        break
-print("Determined rate",k-1)
+        # break
+        if (s>876000):
+            break
+        k = max(2,k-10)
+        flag  = True
 client.close()
+f = open("filerecv.txt","w")
+f.write(ans)
